67df507d4609956c5c8ba7932c807c353fbd67bb
Peter Aaser <peteraaser@gmail.com>
2016-11-10 17:39:35 +0100

    Move ANN from int to Numeric type.

--- src/main/scala/ffANN.scala
+++ src/main/scala/ffANN.scala
@@ -2,11 +2,14 @@ package SHODAN
 
 // todo make generic and maybe also not suck
 object Filters {
-  case class FeedForward(layout: List[Int], bias: List[Int], weights: List[Int]) {
+  case class FeedForward[T](layout: List[Int], bias: List[T], weights: List[T])
+                        (implicit ev: Numeric[T]) {
+
+    import ev._
 
     import FeedForward._
 
-    val activator: Int => Int = λ => λ
+    val activator: T => T = λ => λ
 
     val layers = layout.length
 
@@ -16,47 +19,50 @@ object Filters {
         .map(λ => λ._1 * λ._2)
 
 
-    val biasSlices: List[List[Int]] =
+    val biasSlices: List[List[T]] =
       sliceVector(layout.tail, bias)
 
 
-    val matrixList: List[List[List[Int]]] = {
+    val matrixList: List[List[List[T]]] = {
       val slices = sliceVector(layerSlicePoints, weights)
                               (layout zip slices).map(λ => toMatrix(λ._1, λ._2))
     }.map(_.transpose)
 
 
-    val layerCalculations: List[ List[Int] => List[Int] ] =
+    val layerCalculations: List[ List[T] => List[T] ] =
       (0 until layout.length - 1).toList.map(
         idx => {
-          ((input: List[Int]) => calculateLayerOutput(biasSlices(idx), matrixList(idx), input)
+          ((input: List[T]) => calculateLayerOutput(biasSlices(idx), matrixList(idx), input)
                   .map(activator(_)))
       })
 
 
-    val feed: List[Int] => List[Int] = input =>
+    val feed: List[T] => List[T] = input =>
       (input /: layerCalculations)((λ, f) => f(λ))
 
   }
   case object FeedForward {
 
-    def sliceVector(points: List[Int], victim: List[Int]): List[List[Int]] =
+    def sliceVector[T: Numeric](points: List[Int], victim: List[T]): List[List[T]] =
       (points, victim) match {
         case ((p :: ps), _) => victim.take(p) :: sliceVector(ps, victim.drop(p))
-        case _ => List[List[Int]]()
+        case _ => List[List[T]]()
       }
 
-    def toMatrix(inputs: Int, weights: List[Int]): List[List[Int]] = {
+    def toMatrix[T: Numeric](inputs: Int, weights: List[T]): List[List[T]] = {
       val outputs = weights.length/inputs
       weights.sliding(outputs, outputs).toList
     }
 
-    def calculateLayerOutput(
-        bias: List[Int]
-      , weights: List[List[Int]]
-      , input: List[Int]
+    def calculateLayerOutput[T](
+        bias: List[T]
+      , weights: List[List[T]]
+      , input: List[T]
+    )(implicit ev: Numeric[T]
 
-    ): List[Int] = {
+    ): List[T] = {
+
+      import ev._
 
       val outputs = weights.map(λ => (λ zip input).map(λ => λ._1 * λ._2).sum)
       (outputs zip bias).map(λ => λ._1 + λ._2)
