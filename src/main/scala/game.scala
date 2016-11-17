8cdbd7d5f3edfec62c3b2a54a0a7571e573fab4b
Peter Aaser <peteraaser@gmail.com>
2016-11-17 12:12:46 +0100

    Move game pipe to own file

--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -78,31 +78,3 @@ object wallAvoid {
   def normalizeAngle(a: Double): Double =
     if(a > 2.0*PI) a - 2.0*PI else {if (a < 0) a + 2.0*PI else a}
 }
-
-object agentPipe {
-
-  import fs2._
-
-  import wallAvoid._
-  import Agent._
-
-  def wallAvoidancePipe[F[_]]: Pipe[F, List[Double], List[Double]] = {
-
-    def go(agent: Agent): Handle[F,List[Double]] => Pull[F,List[Double],Unit] = h => {
-      h.receive1 {
-        case(input, h) => {
-          val (nextAgent, sensorData) = updateAgent(agent, input)
-          // println(s"wow game thing, the agent saw $sensorData")
-          Pull.output1(sensorData) >> go(nextAgent)(h)
-        }
-      }
-    }
-
-    val initAgent =
-      Agent(Coord(( wallAvoid.width/2.0), (wallAvoid.height/2.0)), 0.0, 90)
-
-    _.pull(go(initAgent))
-
-  }
-
-}
