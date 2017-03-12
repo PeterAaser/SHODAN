88bc6b5a7f453934b1fdc3e85696f14c9b0afb6a
Peter Aaser <peteraaser@gmail.com>
2017-03-12 15:47:09 +0100

    Add missing file

---
+++ shared/src/main/scala/com/cyborg/params.scala
@@ -0,0 +1,29 @@
+package com.cyborg
+
+import com.typesafe.config._
+import fommil.sjs.FamilyFormats._
+import collection.JavaConverters._
+
+object params {
+
+
+  case class NeuroDataParams(
+    sampleRate: Int
+      , DAQchannels: List[Int]
+      , electrodes: List[Int]
+      , sweepSize: Int
+  )
+  object NeuroDataParams {
+    def apply(c: Config): NeuroDataParams = {
+
+      val d = c.getIntList("DAQchannels").asScala.toList.map(_.toInt)
+      val e = c.getIntList("electrodes").asScala.toList.map(_.toInt)
+
+      NeuroDataParams(c.getInt("sampleRate"), d, e, c.getInt("sweepSize"))
+    }
+  }
+ // object MyJsonProtocol extends DefaultJsonProtocol {
+ //   implicit val modelFormat = jsonFormat4(NeuroDataParams.apply)
+ // }
+
+}
