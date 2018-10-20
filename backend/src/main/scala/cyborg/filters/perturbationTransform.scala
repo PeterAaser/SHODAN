23223d079f9680a2217fe3d39aee52c1a6f15834
Thomas Aven <avensin@hotmail.com>
2018-10-20 19:45:46 +0200

    Implement rate limiting in toStimReq.
    toStimReq should only output FiniteDurations if there has been a
    sufficient change in the channels of the agent -- otherwise it should
    discard the input. Note that the threshold check is done before
    pruning away values outside of the sight range, such that an output
    may consist of only None if the values that trigger an output all lie
    outside this range.

--- backend/src/main/scala/cyborg/filters/perturbationTransform.scala
+++ backend/src/main/scala/cyborg/filters/perturbationTransform.scala
@@ -9,6 +9,7 @@ import scala.concurrent.duration._
 import utilz._
 import fs2._
 import params._
+import bonus._
 
 
 object PerturbationTransform {
@@ -29,68 +30,46 @@ object PerturbationTransform {
   }
 
 
+  /** Checks whether a given threshold for the change of sight range
+    * for an agent is exceeded.
+    */
+  def rangeThreshExceeded(threshold: Double, prev: List[Double], cur: List[Double])
+      : Boolean = {
+    prev.zipWith(cur)((x,y) => math.abs(y-x) >= threshold).foldLeft(false)(_ || _)
+  }
+
+  // (TODO) (thomaav): Think about how min/max ranges should affect this
   /** Transforms agent sight ranges to stimuli pulses. The default
     * transform is a linear transform.
+    *
+    * Note that the output may consist of only None if sight ranges
+    * that trigger a new pulse are outside the min/max ranges of the
+    * agent.
     */
   def toStimReq[F[_]](transform: Double => Option[FiniteDuration] = linearTransform)
       : Pipe[F,List[Double], List[(Int, Option[FiniteDuration])]] = {
 
-    def go(s: Stream[F, List[Double]])
+    def go(prev: List[Double], s: Stream[F, List[Double]])
         : Pull[F, List[(Int, Option[FiniteDuration])], Unit] = {
-      s.pull.uncons.flatMap {
-        case Some((sr,tl)) =>
-          Pull.output1(sr.force.toList.head.zipWithIndex.map(λ => (λ._2, transform(λ._1)))) >> go(tl)
+      s.pull.uncons1.flatMap {
         case None => Pull.done
-      }
-    }
-
-    in => go(in).stream
-  }
-
-
-  def stimuliRequestSink(tolerance: Double)(implicit ec: EC): Sink[IO,List[Double]] = {
-
-    def go(prev: List[Double], s: Stream[IO,List[Double]]): Pull[IO,IO[Unit],Unit] = {
-      s.pull.uncons1 flatMap {
-        case Some((seg,tl)) => {
-
-          val clamped = seg.map(λ => if(λ > 3200.0) 3200.0 else( if(λ < 200.0) 200.0 else λ))
-          val shouldUpdate = {
-
-            val diffExceedsThreshHold = ((prev zip clamped).map { case(old,next) => math.abs(old-next) })
-              .foldLeft(false){ (a,b) => a || (b >= tolerance) }
-
-            val sensorOutOfRange = (prev zip clamped).map {
-              case(old,next) => ((next >= maxDistance) && !(old >= maxDistance))
-            }.foldLeft(false)(_||_)
-            (diffExceedsThreshHold || sensorOutOfRange)
-          }
-
-          // val stimReq = DspCalls.createStimRequests(clamped)
-          val stimReq = IO {say("Firing off an unimplemented stim request, CLOG WARNING")}
-
-          if(shouldUpdate) {
-            Pull.output1(stimReq) >> go(clamped,tl)
-          }
+        case Some((sr,tl)) => {
+          if (rangeThreshExceeded(100.0, prev, sr))
+            Pull.output1(sr.zipWithIndex.map(x => (x._2, transform(x._1)))) >> go(sr, tl)
           else
-            go(prev,tl)
+            go(prev, tl)
         }
+      }
+    }
+
+    def init(s: Stream[F, List[Double]])
+        : Pull[F, List[(Int, Option[FiniteDuration])], Unit] = {
+      s.pull.uncons1.flatMap {
         case None => Pull.done
+        case Some((sr,tl)) => go(sr,tl)
       }
     }
 
-
-    def init(s: Stream[IO,List[Double]]): Pull[IO,IO[Unit],Unit] = {
-      s.pull.uncons1 flatMap {
-        case Some((seg,tl)) => {
-          go(seg, tl)
-        }
-        case None => {
-          Pull.done
-        }
-      }
-    }
-
-    in => init(in).stream.flatMap(Stream.eval).drain.join(100)
+    in => init(in).stream
   }
 }
