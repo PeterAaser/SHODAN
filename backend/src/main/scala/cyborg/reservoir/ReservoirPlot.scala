7659c255580fc7646effcab62ccd53fcab245c4b
Thomas Aven <avensin@hotmail.com>
2018-11-01 19:59:09 +0100

    Update plotting to handle samplerate of 20 000.
    It's still not particularly good, but it seems to keep up. Implementing a
    smoother visualization when resampling or using slower playback can be done.

--- backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
+++ backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
@@ -9,47 +9,46 @@ import scala.concurrent.duration._
 
 object ReservoirPlot {
   /**
-    * Plots an array of floats. Note that this will not currently be
-    * able to keep up with a samplerate of 20 000 -- replaying at
-    * about 1000 should produce smooth results. Consider this a crude
-    * debugging tool.
+    * Plots an array of floats. Consider this a crude debugging tool.
     */
   def plot(stream: Array[Float], samplerate: Int,
-    resolution: FiniteDuration = 0.001.second): Unit = {
+    resolution: FiniteDuration = 0.5.second): Unit = {
     val ticksPerSecond = (1.second/resolution).toInt
     val elementsPerTick = (samplerate/ticksPerSecond)
-    val totalTicks = (stream.length/elementsPerTick)
-    val plottedTicks = (ticksPerSecond)
+    val plottedTicks = ticksPerSecond*3
     val slidingWindowSize = plottedTicks * elementsPerTick
+    var slidingWindow = stream.take(slidingWindowSize)
     var remainingStream = stream.drop(slidingWindowSize)
 
-    val dataSet = new data.time.DynamicTimeSeriesCollection(
+    var dataSet = new data.time.DynamicTimeSeriesCollection(
       1, slidingWindowSize, new data.time.Millisecond())
     dataSet.setTimeBase(new data.time.Millisecond())
-    dataSet.addSeries(stream, 0, "Stream")
-    val ReservoirChart: chart.JFreeChart =
+    dataSet.addSeries(slidingWindow, 0, "Stream")
+    val reservoirChart: chart.JFreeChart =
       chart.ChartFactory.createTimeSeriesChart(
       "SHODAN", "time (undef)", "amplitude", dataSet, true, true, false)
 
-    val plot: chart.plot.XYPlot = ReservoirChart.getXYPlot
+    val plot: chart.plot.XYPlot = reservoirChart.getXYPlot
     val domain = plot.getDomainAxis
     domain.setAutoRange(true)
     domain.setVisible(false)
 
-    val frame = new chart.ChartFrame("SHODAN", ReservoirChart)
+    val frame = new chart.ChartFrame("SHODAN", reservoirChart)
     frame.pack()
     ui.RefineryUtilities.centerFrameOnScreen(frame)
     frame.setVisible(true)
 
     val timer = new Timer(resolution.toMillis.toInt, new ActionListener {
       def actionPerformed(e: ActionEvent): Unit = {
-        val tickData = remainingStream.take(elementsPerTick)
+        slidingWindow = slidingWindow.drop(elementsPerTick) ++ remainingStream.take(elementsPerTick)
         remainingStream = remainingStream.drop(elementsPerTick)
 
-        for (dataPoint <- tickData) {
-          dataSet.advanceTime
-          dataSet.appendData(Array(dataPoint))
-        }
+        dataSet = new data.time.DynamicTimeSeriesCollection(
+          1, slidingWindowSize, new data.time.Millisecond())
+        dataSet.setTimeBase(new data.time.Millisecond())
+        dataSet.addSeries(slidingWindow, 0, "Stream")
+
+        plot.setDataset(dataSet)
       }
     })
 
