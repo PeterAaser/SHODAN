46fe952b5d280bcb053dd5f46d05e30355a196b0
Thomas Aven <avensin@hotmail.com>
2018-11-01 09:54:37 +0100

    Plot dynamic data over time with a sliding window.

--- backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
+++ backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
@@ -5,21 +5,31 @@ import java.awt.event.ActionListener
 import javax.swing.Timer
 import org.jfree._
 
+import scala.concurrent.duration._
+
 object ReservoirPlot {
   /**
     * Plots an array of floats. Note that this currently has no notion
     * of timing, and thus has no dynamic updating.
     */
-  def plot(stream: Array[Float]): Unit = {
+  def plot(stream: Array[Float], samplerate: Int,
+    resolution: FiniteDuration = 0.01.second): Unit = {
+    val ticksPerSecond = (1.second/resolution).toInt
+    val elementsPerTick = (samplerate/ticksPerSecond)
+    val totalTicks = (stream.length/elementsPerTick)
+    val plottedTicks = (ticksPerSecond)*2
+    val slidingWindowSize = plottedTicks * elementsPerTick
+    var remainingStream = stream.drop(slidingWindowSize)
+
     val dataSet = new data.time.DynamicTimeSeriesCollection(
-      1, stream.length, new data.time.Millisecond())
+      1, slidingWindowSize, new data.time.Millisecond())
     dataSet.setTimeBase(new data.time.Millisecond())
     dataSet.addSeries(stream, 0, "Stream")
     val ReservoirChart: chart.JFreeChart =
       chart.ChartFactory.createTimeSeriesChart(
       "SHODAN", "time (undef)", "amplitude", dataSet, true, true, false)
 
-    // (TODO) (thomaav): Find a good way to plot domain (time)
+    // (TODO) (thomaav): Find a good labelling for domain (time)
     val plot: chart.plot.XYPlot = ReservoirChart.getXYPlot
     val domain = plot.getDomainAxis
     domain.setAutoRange(true)
@@ -30,12 +40,17 @@ object ReservoirPlot {
     ui.RefineryUtilities.centerFrameOnScreen(frame)
     frame.setVisible(true)
 
-    // (TODO) (thomaav): Fix me to work with input data
-    val timer = new Timer(1, new ActionListener {
+    // (TODO) (thomaav): Find a way to advance time without stepping
+    // like this
+    val timer = new Timer(resolution.toMillis.toInt, new ActionListener {
       def actionPerformed(e: ActionEvent): Unit = {
-        dataSet.advanceTime
-        val newdater = Array[Float](0.0f)
-        dataSet.appendData(newdater)
+        val tickData = remainingStream.take(elementsPerTick)
+        remainingStream = remainingStream.drop(elementsPerTick)
+
+        for (dataPoint <- tickData) {
+          dataSet.advanceTime
+          dataSet.appendData(Array(dataPoint))
+        }
       }
     })
 
