cdce286b868fd7b92627370a1e6732e97ab906f9
Thomas Aven <avensin@hotmail.com>
2018-11-01 20:41:39 +0100

    Restructure plotting code for easier re-use.

--- backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
+++ backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
@@ -9,49 +9,76 @@ import scala.concurrent.duration._
 
 object ReservoirPlot {
   /**
-    * Plots an array of floats. Consider this a crude debugging tool.
+    * Plots a stream. Consider this a crude debugging tool.
     */
-  def plot(stream: Array[Float], samplerate: Int,
-    resolution: FiniteDuration = 0.5.second): Unit = {
+  class TimeSeriesPlot(stream: Array[Float], samplerate: Int,
+    resolution: FiniteDuration = 0.5.second) {
     val ticksPerSecond = (1.second/resolution).toInt
     val elementsPerTick = (samplerate/ticksPerSecond)
     val plottedTicks = ticksPerSecond*3
     val slidingWindowSize = plottedTicks * elementsPerTick
+    var minRangeValue = -500
+    var maxRangeValue = 500
+
+    // The sliding window ticks over the stream according to the
+    // variables above.
     var slidingWindow = stream.take(slidingWindowSize)
     var remainingStream = stream.drop(slidingWindowSize)
 
-    var dataSet = new data.time.DynamicTimeSeriesCollection(
+    var dataset = new data.time.DynamicTimeSeriesCollection(
       1, slidingWindowSize, new data.time.Millisecond())
-    dataSet.setTimeBase(new data.time.Millisecond())
-    dataSet.addSeries(slidingWindow, 0, "Stream")
+    dataset.setTimeBase(new data.time.Millisecond())
+    dataset.addSeries(slidingWindow, 0, "Stream")
+
+    // These are the actual chart and plot that are used to modify
+    // what's displayed directly. Not very FP friendly.
     val reservoirChart: chart.JFreeChart =
       chart.ChartFactory.createTimeSeriesChart(
-      "SHODAN", "time (undef)", "amplitude", dataSet, true, true, false)
-
+        "SHODAN", "time (undef)", "amplitude", dataset, true, true, false)
     val plot: chart.plot.XYPlot = reservoirChart.getXYPlot
-    val domain = plot.getDomainAxis
-    domain.setAutoRange(true)
-    domain.setVisible(false)
-
     val frame = new chart.ChartFrame("SHODAN", reservoirChart)
-    frame.pack()
-    ui.RefineryUtilities.centerFrameOnScreen(frame)
-    frame.setVisible(true)
 
-    val timer = new Timer(resolution.toMillis.toInt, new ActionListener {
-      def actionPerformed(e: ActionEvent): Unit = {
-        slidingWindow = slidingWindow.drop(elementsPerTick) ++ remainingStream.take(elementsPerTick)
-        remainingStream = remainingStream.drop(elementsPerTick)
+    def hideDomain: Unit = {
+      val domain = plot.getDomainAxis
+      domain.setAutoRange(true)
+      domain.setVisible(false)
+    }
 
-        dataSet = new data.time.DynamicTimeSeriesCollection(
-          1, slidingWindowSize, new data.time.Millisecond())
-        dataSet.setTimeBase(new data.time.Millisecond())
-        dataSet.addSeries(slidingWindow, 0, "Stream")
+    def setRange(min: Int, max: Int): Unit = {
+      minRangeValue = min
+      maxRangeValue = max
+      val range = plot.getRangeAxis
+      range.setRange(minRangeValue, maxRangeValue)
+    }
 
-        plot.setDataset(dataSet)
-      }
-    })
+    def updateDataset: Unit = {
+      slidingWindow = slidingWindow.drop(elementsPerTick) ++
+            remainingStream.take(elementsPerTick)
+      remainingStream = remainingStream.drop(elementsPerTick)
 
-    timer.start
+      dataset = new data.time.DynamicTimeSeriesCollection(
+        1, slidingWindowSize, new data.time.Millisecond())
+      dataset.setTimeBase(new data.time.Millisecond())
+      dataset.addSeries(slidingWindow, 0, "Stream")
+
+      plot.setDataset(dataset)
+    }
+
+    def show: Unit = {
+      hideDomain
+      setRange(minRangeValue, maxRangeValue)
+
+      frame.pack()
+      ui.RefineryUtilities.centerFrameOnScreen(frame)
+      frame.setVisible(true)
+
+      val timer = new Timer(resolution.toMillis.toInt, new ActionListener {
+        def actionPerformed(e: ActionEvent): Unit = {
+          updateDataset
+        }
+      })
+
+      timer.start
+    }
   }
 }
