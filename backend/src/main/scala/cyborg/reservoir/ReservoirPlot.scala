20ec72a3fa879e819514772d7f2bdfce941945aa
Thomas Aven <avensin@hotmail.com>
2018-11-11 13:13:51 +0100

    Set some standard config values for ReservoirPlot.

--- backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
+++ backend/src/main/scala/cyborg/reservoir/ReservoirPlot.scala
@@ -8,6 +8,28 @@ import org.jfree._
 import scala.concurrent.duration._
 
 object ReservoirPlot {
+  object PlotConfig {
+    val visibleTicks = 3
+    val lowerBound = -500
+    val upperBound = 500
+  }
+
+  /**
+   * Helper function for pre-calculating the size of the sliding
+   * window _before_ actually initializing a plot. Useful for
+   * initializing a stream of zero-values to initialize
+   * TimeSeriesPlot.
+   */
+  def getSlidingWindowSize(samplerate: Int,
+    resolution: FiniteDuration = 0.5.second): Int = {
+    val ticksPerSecond = (1.second/resolution).toInt
+    val elementsPerTick = (samplerate/ticksPerSecond)
+    val plottedTicks = ticksPerSecond*PlotConfig.visibleTicks
+    val slidingWindowSize = plottedTicks * elementsPerTick
+
+    slidingWindowSize
+  }
+
   /**
     * Plots a stream. Consider this a crude debugging tool.
     */
@@ -15,10 +37,10 @@ object ReservoirPlot {
     resolution: FiniteDuration = 0.5.second) {
     val ticksPerSecond = (1.second/resolution).toInt
     val elementsPerTick = (samplerate/ticksPerSecond)
-    val plottedTicks = ticksPerSecond*3
+    val plottedTicks = ticksPerSecond*PlotConfig.visibleTicks
     val slidingWindowSize = plottedTicks * elementsPerTick
-    var minRangeValue = -500
-    var maxRangeValue = 500
+    var minRangeValue = PlotConfig.lowerBound
+    var maxRangeValue = PlotConfig.upperBound
 
     // The sliding window ticks over the stream according to the
     // variables above.
