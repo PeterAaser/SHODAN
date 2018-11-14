1a26f3d719395c6dccd374a730017881171aafd5
Thomas Aven <avensin@hotmail.com>
2018-11-14 15:15:08 +0100

    Add crufty RBN generator before starting clean up.

--- backend/src/main/scala/cyborg/reservoir/Reservoir.scala
+++ backend/src/main/scala/cyborg/reservoir/Reservoir.scala
@@ -145,6 +145,24 @@ object RBNContext {
     }
   }
 
+  object RBNs {
+    def createRandomRBN: RBN = {
+      import scala.util.Random
+
+      // (TODO): Use proper functional RNG library
+      val numNodes = 60
+      val nodes = (0 until numNodes).toList
+      val state = List.fill(numNodes)(math.random < 0.5)
+      val edges = nodes.map{i =>
+        Random.shuffle(nodes.filter(n => n != i)).take(2).toList
+      }
+
+      RBN(state, edges, XOR)
+    }
+
+    val randomRBN = createRandomRBN
+  }
+
   /**
     * A simple rule to update an arbitrary RBN by giving nodes a new
     * state given by folding its neighbors over XOR.
