a281458f83060434cf8802cb68c3ed373fb1c594
peteraa <peteraaser@gmail.com>
2018-11-16 13:27:02 +0100

    Add lipstick to the pig

--- shared/src/main/scala/cyborg/shared/mcsChannelMap.scala
+++ shared/src/main/scala/cyborg/shared/mcsChannelMap.scala
@@ -1,139 +1,47 @@
 package cyborg
 
+/**
+  In germany they count ...differently
+ */
 object mcsChannelMap {
 
 
-  val MEAMEtoSHODAN = Map(
-    12 ->  20,
-    13 ->  18,
-    14 ->  15,
-    15 ->  14,
-    16 ->  11,
-    17 ->  9,
-    21 ->  23,
-    22 ->  21,
-    23 ->  19,
-    24 ->  16,
-    25 ->  13,
-    26 ->  10,
-    27 ->  8,
-    28 ->  6,
-    31 ->  25,
-    32 ->  24,
-    33 ->  22,
-    34 ->  17,
-    35 ->  12,
-    36 ->  7,
-    37 ->  5,
-    38 ->  4,
-    41 ->  28,
-    42 ->  29,
-    43 ->  27,
-    44 ->  26,
-    45 ->  3,
-    46 ->  2,
-    47 ->  0,
-    48 ->  1,
-    51 ->  31,
-    52 ->  30,
-    53 ->  32,
-    54 ->  33,
-    55 ->  56,
-    56 ->  57,
-    57 ->  59,
-    58 ->  58,
-    61 ->  34,
-    62 ->  35,
-    63 ->  37,
-    64 ->  42,
-    65 ->  47,
-    66 ->  52,
-    67 ->  54,
-    68 ->  55,
-    71 ->  36,
-    72 ->  38,
-    73 ->  40,
-    74 ->  43,
-    75 ->  46,
-    76 ->  49,
-    77 ->  51,
-    78 ->  53,
-    82 ->  39,
-    83 ->  41,
-    84 ->  44,
-    85 ->  45,
-    86 ->  48,
-    87 ->  50)
-
-  val SHODANtoMEAME = MEAMEtoSHODAN.toList.map(x => (x._2, x._1)).toMap
-
-  val getMCSchannel = Map(
-    0  -> MEAMEtoSHODAN(21),
-    1  -> MEAMEtoSHODAN(31),
-    2  -> MEAMEtoSHODAN(41),
-    3  -> MEAMEtoSHODAN(51),
-    4  -> MEAMEtoSHODAN(61),
-    5  -> MEAMEtoSHODAN(71),
-
-    6  -> MEAMEtoSHODAN(12),
-    7  -> MEAMEtoSHODAN(22),
-    8  -> MEAMEtoSHODAN(32),
-    9  -> MEAMEtoSHODAN(42),
-    10 -> MEAMEtoSHODAN(52),
-    11 -> MEAMEtoSHODAN(62),
-    12 -> MEAMEtoSHODAN(72),
-    13 -> MEAMEtoSHODAN(82),
-
-    14  -> MEAMEtoSHODAN(13),
-    15  -> MEAMEtoSHODAN(23),
-    16  -> MEAMEtoSHODAN(33),
-    17  -> MEAMEtoSHODAN(43),
-    18  -> MEAMEtoSHODAN(53),
-    19  -> MEAMEtoSHODAN(63),
-    20  -> MEAMEtoSHODAN(73),
-    21  -> MEAMEtoSHODAN(83),
-
-    22  -> MEAMEtoSHODAN(14),
-    23  -> MEAMEtoSHODAN(24),
-    24  -> MEAMEtoSHODAN(34),
-    25  -> MEAMEtoSHODAN(44),
-    26  -> MEAMEtoSHODAN(54),
-    27  -> MEAMEtoSHODAN(64),
-    28  -> MEAMEtoSHODAN(74),
-    29  -> MEAMEtoSHODAN(84),
-
-    30  -> MEAMEtoSHODAN(15),
-    31  -> MEAMEtoSHODAN(25),
-    32  -> MEAMEtoSHODAN(35),
-    33  -> MEAMEtoSHODAN(45),
-    34  -> MEAMEtoSHODAN(55),
-    35  -> MEAMEtoSHODAN(65),
-    36  -> MEAMEtoSHODAN(75),
-    37  -> MEAMEtoSHODAN(85),
-
-    38  -> MEAMEtoSHODAN(16),
-    39  -> MEAMEtoSHODAN(26),
-    40  -> MEAMEtoSHODAN(36),
-    41  -> MEAMEtoSHODAN(46),
-    42  -> MEAMEtoSHODAN(56),
-    43  -> MEAMEtoSHODAN(66),
-    44  -> MEAMEtoSHODAN(76),
-    45  -> MEAMEtoSHODAN(86),
-
-    46  -> MEAMEtoSHODAN(17),
-    47  -> MEAMEtoSHODAN(27),
-    48  -> MEAMEtoSHODAN(37),
-    49  -> MEAMEtoSHODAN(47),
-    50  -> MEAMEtoSHODAN(57),
-    51  -> MEAMEtoSHODAN(67),
-    52  -> MEAMEtoSHODAN(77),
-    53  -> MEAMEtoSHODAN(87),
-
-    54  -> MEAMEtoSHODAN(28),
-    55  -> MEAMEtoSHODAN(38),
-    56  -> MEAMEtoSHODAN(48),
-    57  -> MEAMEtoSHODAN(58),
-    58  -> MEAMEtoSHODAN(68),
-    59  -> MEAMEtoSHODAN(78)
+  /**
+    Maps the visual layout of MCS channels to the data stream
+    For instance, to get what MCS labels as channel 21 we need to
+    pick out the 23rd element in the datastream
+    */
+  val MCStoSHODAN = Map(
+                12 -> 20,   13 -> 18,   14 -> 15,   15 -> 14,   16 -> 11,   17 -> 9,
+    21 -> 23,   22 -> 21,   23 -> 19,   24 -> 16,   25 -> 13,   26 -> 10,   27 -> 8,    28 -> 6,
+    31 -> 25,   32 -> 24,   33 -> 22,   34 -> 17,   35 -> 12,   36 -> 7,    37 -> 5,    38 -> 4,
+    41 -> 28,   42 -> 29,   43 -> 27,   44 -> 26,   45 -> 3,    46 -> 2,    47 -> 0,    48 -> 1,
+    51 -> 31,   52 -> 30,   53 -> 32,   54 -> 33,   55 -> 56,   56 -> 57,   57 -> 59,   58 -> 58,
+    61 -> 34,   62 -> 35,   63 -> 37,   64 -> 42,   65 -> 47,   66 -> 52,   67 -> 54,   68 -> 55,
+    71 -> 36,   72 -> 38,   73 -> 40,   74 -> 43,   75 -> 46,   76 -> 49,   77 -> 51,   78 -> 53,
+                82 -> 39,   83 -> 41,   84 -> 44,   85 -> 45,   86 -> 48,   87 -> 50
   )
+
+
+  /**
+    A map between channel visualizations on SHODAN and the corresponding MCS channel
+    Channel 0 for SHODAN corresponds to channel 21 in the MCS viz
+    */
+  val SHODANvizToMCS = Map(
+                0  -> 21,   1  -> 31,   2  -> 41,   3  -> 51,   4  -> 61,   5  -> 71,
+    6  -> 12,   7  -> 22,   8  -> 32,   9  -> 42,   10 -> 52,   11 -> 62,   12 -> 72,   13 -> 82,
+    14 -> 13,   15 -> 23,   16 -> 33,   17 -> 43,   18 -> 53,   19 -> 63,   20 -> 73,   21 -> 83,
+    22 -> 14,   23 -> 24,   24 -> 34,   25 -> 44,   26 -> 54,   27 -> 64,   28 -> 74,   29 -> 84,
+    30 -> 15,   31 -> 25,   32 -> 35,   33 -> 45,   34 -> 55,   35 -> 65,   36 -> 75,   37 -> 85,
+    38 -> 16,   39 -> 26,   40 -> 36,   41 -> 46,   42 -> 56,   43 -> 66,   44 -> 76,   45 -> 86,
+    46 -> 17,   47 -> 27,   48 -> 37,   49 -> 47,   50 -> 57,   51 -> 67,   52 -> 77,   53 -> 87,
+                54 -> 28,   55 -> 38,   56 -> 48,   57 -> 58,   58 -> 68,   59 -> 78
+  )
+
+
+  /**
+    Gets the corresponding segment offset for a SHODAN channel. For SHODAN channel 13, the corresponding
+    MCS channel is 82, which in turn has offset 39 in the datastream
+    */
+  val getMCSchannel = SHODANvizToMCS.toList.map{ case(idx, channel) => (idx, MCStoSHODAN(channel)) }.toMap
 }
